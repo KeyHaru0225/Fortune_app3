6ecb9bec1edb64cd3e1fafaa2064491ab205f447
Kawase Rina <lina.private.tamagoyaki@gmail.com>
2021-06-14 21:43:42 +0900

    とりあえず完成？

--- app/src/main/java/to/msn/wings/fortune_app3/MainActivity.java
+++ app/src/main/java/to/msn/wings/fortune_app3/MainActivity.java
@@ -7,8 +7,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import java.util.Date;
+import java.util.Random;
 
 // TODO 画面固定を追加　済
 // TODO トースト表示「おみくじと入力して下さい」を追加 済
@@ -25,47 +27,38 @@ public class MainActivity extends AppCompatActivity {
 
     // ボタンクリック時の処理定義
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.btnCurrent);
-        btn.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        TextView txt = findViewById(R.id.txtResult);
-                        txt.setText(answer);
-                    }
-                }
-        );
-        // ラムダ式
-        btn.setOnClickListener(view -> {
-            TextView txt = findViewById(R.id.txtResult);
-            txt.setText(answer);
-        });
-    }
-
-
-    // おみくじ
-    private EditText mInputMessage;
-    private Button mSendMessage;
-    private TextView mUserMessage;
-    private String answer;
-
-    // Toast表示  //ボタンをクリックしたら表示される
-    public void btnCurrent_onClick(View view) {
+    protected void onCreate(Bundle savedInstanceState) {  //  135
+        super.onCreate(savedInstanceState); //135
+        setContentView(R.layout.activity_main); //135
 
         mInputMessage = (EditText) findViewById(R.id.user_message); //ユーザーが入力するフィールド
         mSendMessage = (Button) findViewById(R.id.btnCurrent);      //SENDボタン
         mUserMessage = (TextView) findViewById(R.id.txtResult);  //ユーザーが入力した内容
+        mCpuMessage = (TextView) findViewById(R.id.cpu_message); //コンピューターからの応答
+    }
+
+    // おみくじ
+    private EditText mInputMessage; //135
+    private Button mSendMessage;
+    private TextView mUserMessage;
+    private TextView mCpuMessage;
+
+
+    public void btnCurrent_onClick(View view) { //147
+        EditText txtName = findViewById(R.id.user_message);
+        TextView txtResult = findViewById(R.id.txtResult);
+
+
 
         // TextView txt = findViewById(R.id.user_message);
         //        txt.setText(toastMessage);
-        if(view.equals(mSendMessage)) {
+        if(view.equals(mSendMessage)) { //147
             // SENDボタンが押された時の処理
-            String inputText = mInputMessage.getText().toString();
+            String inputText = mInputMessage.getText().toString(); //147
+            String answer; //147
 
-            mUserMessage.setText(inputText);
+
+            mUserMessage.setText(inputText); //147
             if (inputText.contains("おみくじ")) {
                 double random = Math.random() * 5d;
                 if (random < 1d) {
@@ -80,14 +73,34 @@ public class MainActivity extends AppCompatActivity {
                     answer = "大吉";
                 }
             } else {
-                System.out.println("「おみくじ」と入力して下さい");
+                answer = "「おみくじ」と入力してください";
             }
+            mCpuMessage.setText(answer);
+            mInputMessage.setText("");
+
+
         }
+        // Toast表示  //ボタンをクリックしたら表示される
         String toastMessage = "良い結果は出ましたか？";
         Toast toast = Toast.makeText(
                 MainActivity.this, toastMessage, Toast.LENGTH_LONG);
         toast.show();
     }
+        //Button btn = findViewById(R.id.btnCurrent);
+        //btn.setOnClickListener(
+                //new View.OnClickListener() {
+                    //public void btnCurrent_onClick(View view) {
+                        //TextView txt = findViewById(R.id.txtResult);
+                        //txt.setText(answer);
+                    //}
+                //}
+        //);
+
+
+
+
+
+
 
     // 画面が破棄される前に状態を保存
     @Override
